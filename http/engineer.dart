2b65c0031da0c2f86e2917868b1f46b2fc6aa9ce
Millenniar Studios <121758336+MillenniarSt@users.noreply.github.com>
2024-06-14 09:31:46 +0200

    Update engineer.dart

--- http/engineer.dart
+++ http/engineer.dart
@@ -1,26 +1,23 @@
-import 'package:shelf/shelf.dart';
+import 'package:http/htttp.dart' as http;
 
 import '../data/database.dart';
 
 class EngineerHttp {
 
   final String id;
+  final String baseUrl;
 
-  EngineerHttp(this.id);
+  EngineerHttp(this.id, this.baseUrl);
 
-  Future<void> send(String url, json) async {
-    if(json is JsonWritable) {
-      json = json.toJson();
+  Future<dynamic> post(String url, data) async {
+    if(data is JsonWritable) {
+      data = json.toJson();
     }
-    //TODO
-  }
-
-  Future<dynamic> get(String url, json) async {
-    if(json is JsonWritable) {
-      json = json.toJson();
-    }
-    //TODO
-    throw UnimplementedError();
+    return await http.post(
+        Uri.parse("$baseUrl/$url"), 
+        headers: {"Content-Type": "application/json"},
+        body: json.encode(data)
+    );
   }
 
   void listen(String url, Function(Response) onReceived) {
